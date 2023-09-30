c0d910e8326e15d8a879a05c976c61c4c5948f87
Supun Thilakarathne <thilakarathnesupun@gmail.com>
2023-09-30 11:14:02 +0530

    Land Serivce

---
+++ Core/Services/Nwsdb.Web.Api/Services/Foundations/Lands/LandService.cs
@@ -0,0 +1,13 @@
+﻿//-------------------------------------------------------------------------------------
+// Copyright (C) 2023, by National Water Supply and Drain Board. All right reserved.
+// The information and source code contained herein is the exclusive property of 
+// NWSDB and may not be disclosed, examined or reproduced in whole or in part without
+// explicit written authorization from NWSDB ------------------------------------------
+//-------------------------------------------------------------------------------------
+
+namespace Nwsdb.Web.Api.Services.Foundations.Lands
+{
+    public class LandService
+    {
+    }
+}
