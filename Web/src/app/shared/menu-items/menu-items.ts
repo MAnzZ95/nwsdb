76695172967cfb1b4bed50b8cbbfd303364d8a7a
Supun Thilakarathne <thilakarathnesupun@gmail.com>
2023-12-16 23:42:30 +0530

    shared components

--- Web/src/app/shared/menu-items/menu-items.ts
+++ Web/src/app/shared/menu-items/menu-items.ts
@@ -5,51 +5,26 @@ export interface Menu {
   name: string;
   type: string;
   icon: string;
+  children?: Menu[] | undefined;
 }
 
 const MENUITEMS = [
   { state: 'dashboard', name: 'Dashboard', type: 'link', icon: 'av_timer' },
   { state: 'land', type: 'link', name: 'Lands', icon: 'crop_7_5' },
-  { state: 'grid', type: 'link', name: 'Grid List', icon: 'view_comfy' },
-  { state: 'lists', type: 'link', name: 'Lists', icon: 'view_list' },
-  { state: 'menu', type: 'link', name: 'Menu', icon: 'view_headline' },
-  { state: 'tabs', type: 'link', name: 'Tabs', icon: 'tab' },
-  { state: 'stepper', type: 'link', name: 'Stepper', icon: 'web' },
-  {
-    state: 'expansion',
-    type: 'link',
-    name: 'Expansion Panel',
-    icon: 'vertical_align_center'
-  },
-  { state: 'chips', type: 'link', name: 'Chips', icon: 'vignette' },
-  { state: 'toolbar', type: 'link', name: 'Toolbar', icon: 'voicemail' },
-  {
-    state: 'progress-snipper',
-    type: 'link',
-    name: 'Progress snipper',
-    icon: 'border_horizontal'
-  },
-  {
-    state: 'progress',
-    type: 'link',
-    name: 'Progress Bar',
-    icon: 'blur_circular'
-  },
-  {
-    state: 'dialog',
-    type: 'link',
-    name: 'Dialog',
-    icon: 'assignment_turned_in'
-  },
-  { state: 'tooltip', type: 'link', name: 'Tooltip', icon: 'assistant' },
-  { state: 'snackbar', type: 'link', name: 'Snackbar', icon: 'adb' },
-  { state: 'slider', type: 'link', name: 'Slider', icon: 'developer_mode' },
-  {
-    state: 'slide-toggle',
-    type: 'link',
-    name: 'Slide Toggle',
-    icon: 'all_inclusive'
-  }
+  { state: 'rsc', type: 'link', name: 'RS Center', icon: 'view_comfy' },
+  { state: 'rmo', type: 'link', name: 'RM Office', icon: 'view_list' },
+  { state: 'wss', type: 'link', name: 'WS Schemes', icon: 'view_headline' },
+  { state: 'user', type: 'link', name: 'Users', icon: 'tab' },
+  { 
+    state: 'test', 
+    type: 'expandable', 
+    name: 'Stepper', 
+    icon: 'web',
+    children: [
+      { state: 'child1', type: 'link', name: 'Child 1', icon: 'child_care' },
+      { state: 'child2', type: 'link', name: 'Child 2', icon: 'child_care' }
+    ]
+   }
 ];
 
 @Injectable()
